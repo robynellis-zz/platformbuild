0acd7f5d66d71aa2b1b1f54db6ddf0c4c52a31be
Robyn Ellis <roellis@redhat.com>
2021-01-26 20:35:47 -0500

    more small stuff

--- ocs/0-commands.sh
+++ ocs/0-commands.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+
+####label your worker nodes
+####the example below will label your main 3 workers.  adjust as needed.####
 for node in {0..2}
 do
-  oc label nodes worker${node}.kvirt.aliveagain.me cluster.ocs.openshift.io/openshift-storage=''
+  oc label nodes worker${node}.cluster.sample.com cluster.ocs.openshift.io/openshift-storage=''
 done
+
+####Install local storage operator
+
+####Install openshift container storage operator
+
+####Configure local-storage 
