036da7e1ffc83922997c4e25859af5a60eab7547
Robyn Ellis <robynellis@corp.aliveagain.me@bastion.dev.aliveagain.me>
2021-06-30 15:12:08 -0400

    starting to add in my disconnected stuff

---
+++ templates/disconected/start_registry.sh
@@ -0,0 +1,11 @@
+podman run -d --name local-registry -p 5000:5000 \
+-v /opt/registry/data:/var/lib/registry:z \
+-v /opt/registry/auth:/auth:z \
+-e "REGISTRY_AUTH=htpasswd" \
+-e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" \
+-e "REGISTRY_HTTP_SECRET=WhateverYouWantToPutHere" \
+-e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+-v /opt/registry/certs:/certs:z \
+-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
+-e REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
+docker.io/library/registry:2
